1133f3341dcc9153c09474693c4f6f9181eb710f
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-09-04 21:43:09 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,15 @@
+let name = "Romar";
+let age = 21;
+let isStudent = true;
+
+console.log(name, age, isStudent);
+console.log(typeof name, typeof age, typeof isStudent);
+
+let a = 10, b = 5;
+console.log("Add:", a + b);
+console.log("Subtract:", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
+
+console.log('"5" == 5 →', "5" == 5);
+console.log('"5" === 5 →', "5" === 5);
