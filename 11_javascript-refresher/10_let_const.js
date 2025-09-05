077a76073aae3c840e1cc24d8940595cbac3ca3e
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-09-05 12:04:06 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,12 @@
+var name = "Romar"; 
+var age = 22;
+age = 20;
+console.log("var:", name, age);
+
+let city = "Tarlac";
+city = "Tarlac City"; 
+console.log("let:", city);
+
+const country = "Philippines";
+
+console.log("const:", country);
