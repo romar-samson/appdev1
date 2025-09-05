4d6f0884a85a8c1ae8c6b12d4acf32e7edf44104
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-09-05 12:15:52 +0800

    Add classes and inheritance activity

---
+++ 11_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+const student = new Student("Romar");
+student.sayHello();
+student.study();
