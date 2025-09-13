53d984f9ccb812bc44b33c9452d2f5d405bf0170
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-09-13 10:25:02 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton />
-      </div>
+
     </>
   )
 }
@@ -17,6 +11,15 @@ function App() {
 function MyButton() {
   return (
     <button>I'm a button</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
   );
 }
 
