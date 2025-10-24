25b91a1400e2843f3c74e3ea0fe9dc01b10c04c7
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-10-24 22:11:11 +0800

    Done with how to write my first React component

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+function Profile() {
   return (
-    <>
-      
-    </>
-  )
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
 }
 
-export default App
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
