4ebc4dcbd589e24393faf5084f60b10ec54606d5
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-10-24 23:17:26 +0800

    Done with how to avoid confusing bugs by keeping components pure

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -38,26 +38,18 @@ function Card({ children }) {
   );
 }
 
+let guest = 0;
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-  return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
-  );
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
