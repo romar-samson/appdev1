005859c5fd046b44509b36a319b8a142923d7b20
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-10-24 22:57:54 +0800

    Done with how to configure components with props

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,36 +1,46 @@
-function Profile() {
+import { getImageUrl } from "./utils.js";
+
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "white",
+  },
+};
+
+function Avatar({ person, size }) {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
     />
   );
 }
 
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'white'
-  }
-};
 
-
-export default function TodoList() {
+function Card({ children }) {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
+    <div className="card">
+      {children}
     </div>
+  );
+}
+
+
+export default function Profile() {
+  return (
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
+      />
+    </Card>
   );
 }
 
--- react-describing-ui/src/Gallery.jsx
+++ react-describing-ui/src/Gallery.jsx
@@ -1,4 +1,4 @@
-import Profile from './Profile.jsx';
+import Profile from "./Profile.jsx";
 
 export default function Gallery() {
   return (
