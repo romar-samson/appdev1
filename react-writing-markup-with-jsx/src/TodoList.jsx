9a87ffed4fbf81b2603c99df3e0bdeb3e5f6a2b2
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-11-09 18:39:02 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/TodoList.jsx
+++ react-writing-markup-with-jsx/src/TodoList.jsx
@@ -1,6 +1,6 @@
 export default function TodoList() {
   return (
-    <div>
+    <>
       <h1>Hedy Lamarr's Todos</h1>
       <img 
         src="https://i.imgur.com/yXOvdOSs.jpg" 
@@ -12,6 +12,6 @@ export default function TodoList() {
         <li>Rehearse a movie scene</li>
         <li>Improve the spectrum technology</li>
       </ul>
-    </div>
+    </>
   );
 }
