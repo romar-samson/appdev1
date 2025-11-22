b3dc399a22d065fa3dc06e327e55f90879367620
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-11-22 21:52:40 +0800

    Done with passing strings with quotes

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+export default function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
   return (
-    <>
-      
-    </>
-  )
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
 }
-
-export default App
